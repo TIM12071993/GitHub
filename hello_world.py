9d68e445b54d74de7523b865af9d3d2b812c2974
Tim <Artem.Mansurkhodzhaev@mpibpc.mpg.de>
2018-06-11 17:20:34 +0200

    added one more line

--- hello_world.py
+++ hello_world.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
    print("Starting to understand Git. This is already my second commit")
    print("Hello World again")
    print("I think I already understand the basic concept of add and commit. Really?")
+   print("sup")
    monkey.print_monkeys([0,1,2,3,4,5])
+   
    
    
