772d501873b15732db8b7c80eebb5218fa7cb1dd
Tim <Artem.Mansurkhodzhaev@mpibpc.mpg.de>
2018-06-11 16:02:22 +0200

    your commit message

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,7 @@
 if __name__ == "__main__":
    print("Hello World")
    print("Starting to understand Git. This is already my second commit")
+   print("Hello World again")
+   print("I think I already understand the basic concept of add and commit. Really?")
+   
    
--- hello_world_new.py
+++
@@ -1,4 +0,0 @@
-if __name__ == "__main__":
-   print("Hello World")
-   print("Starting to understand Git. This is already my second commit")
-   
