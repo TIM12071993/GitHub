57ea173b75a264f448431f1da180ffb598d30932
Tim <Artem.Mansurkhodzhaev@mpibpc.mpg.de>
2018-06-12 11:18:26 +0200

    added new line

--- hello_world.py
+++ hello_world.py
@@ -1,19 +1,21 @@
-import monkey
 import sys
+import monkey
 if __name__ == "__main__":
    print("Hello World")
    print("Starting to understand Git. This is already my second commit")
    print("Hello World again")
    print("I think I already understand the basic concept of add and commit. Really?")
+   print("sup")
    monkey.print_monkeys([0,1,2,3,4,5])
    def print_palmtrees(num):
-       for i in range(num):
-           sys.stdout.write(' /|\\')
-       sys.stdout.write('\n')
-       for i in range (num):
-           sys.stdout.write('  | ')
-       sys.stdout.write('\n\n')
+        for i in range(num):
+            sys.stdout.write(' /|\\')
+        sys.stdout.write('\n')
+        for i in range(num):
+            sys.stdout.write('  | ')
+        sys.stdout.write('\n\n')
    print_palmtrees(7)
-        
+   print ("check")
+   
    
    
